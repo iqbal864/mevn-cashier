ecd4d10a7e7c2a437f51d410f9f4a79df80bb9db
muhamad iqbal falah nurrohmat <muhamadiqbalfalah864@gmail.com>
2022-11-18 11:25:55 +0700

    memperbaiki tampilan dan menambahkan dummy produk

---
+++ components/Produk.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-row>
+    <v-col lg="2" md="3" sm="4" xs="6" v-for="(product, index) in data_product">
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-card ripple="true" class="produk_card" v-bind="attrs" v-on="on">
+            <v-card-actions>
+              <v-img :src="require(`@/assets/images/produk/convert/${product.thumbnail}`)"></v-img>
+            </v-card-actions>
+            <v-card-text align="center" class="produk_title">
+              {{ product.title }}
+            </v-card-text>
+          </v-card>
+        </template>
+        <span> {{ product.title }} </span>
+      </v-tooltip>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+
+export default ({
+  data() {
+    return {
+      data_product: [
+        { id: 1, title: "Bakso", thumbnail: "bakso.jpg", price: 10000, categoryId: 1 },
+        { id: 2, title: "Bakso SUper", thumbnail: "bakso-super.jpg", price: 20000, categoryId: 1 },
+        { id: 3, title: "Mie Bakso", thumbnail: "mie-bakso.jpg", price: 15000, categoryId: 1 },
+        { id: 4, title: "Mie Ayam", thumbnail: "mie-ayam.jpg", price: 15000, categoryId: 1 },
+        { id: 5, title: "Soto Ayam", thumbnail: "soto-ayam.jpg", price: 18000, categoryId: 1 },
+        { id: 6, title: "Nasi Goreng", thumbnail: "nasi-goreng.jpg", price: 13000, categoryId: 1 },
+        { id: 7, title: "Nasi Goreng Spesial", thumbnail: "bakso.jpg", price: 18000, categoryId: 1 },
+        { id: 8, title: "Nasi Ayam", thumbnail: "nasi-ayam.jpg", price: 17000, categoryId: 1 },
+        { id: 9, title: "Ayam Geprek", thumbnail: "ayam-geprek.jpg", price: 15000, categoryId: 1 },
+        { id: 10, title: "Gado gado", thumbnail: "gado-gado.jpg", price: 12000, categoryId: 1 },
+        { id: 11, title: "Es Teh", thumbnail: "es-teh.jpg", price: 5000, categoryId: 2 },
+        { id: 12, title: "Teh Panas", thumbnail: "teh-panas.jpg", price: 5000, categoryId: 2 },
+        { id: 13, title: "Nutrisari", thumbnail: "nutrisari.jpg", price: 5000, categoryId: 2 },
+        { id: 14, title: "Nutrisari Susu", thumbnail: "nutrisari-susu.jpg", price: 8000, categoryId: 2 },
+        { id: 15, title: "Fanta Susu", thumbnail: "fanta-susu.jpg", price: 8000, categoryId: 2 },
+        { id: 16, title: "Air Mineral", thumbnail: "air-mineral.jpg", price: 3000, categoryId: 2 },
+
+        { id: 1, title: "Bakso", thumbnail: "bakso.jpg", price: 10000, categoryId: 1 },
+        { id: 2, title: "Bakso SUper", thumbnail: "bakso-super.jpg", price: 20000, categoryId: 1 },
+        { id: 3, title: "Mie Bakso", thumbnail: "mie-bakso.jpg", price: 15000, categoryId: 1 },
+        { id: 4, title: "Mie Ayam", thumbnail: "mie-ayam.jpg", price: 15000, categoryId: 1 },
+        { id: 5, title: "Soto Ayam", thumbnail: "soto-ayam.jpg", price: 18000, categoryId: 1 },
+        { id: 6, title: "Nasi Goreng", thumbnail: "nasi-goreng.jpg", price: 13000, categoryId: 1 },
+        { id: 7, title: "Nasi Goreng Spesial", thumbnail: "bakso.jpg", price: 18000, categoryId: 1 },
+        { id: 8, title: "Nasi Ayam", thumbnail: "nasi-ayam.jpg", price: 17000, categoryId: 1 },
+        { id: 9, title: "Ayam Geprek", thumbnail: "ayam-geprek.jpg", price: 15000, categoryId: 1 },
+        { id: 10, title: "Gado gado", thumbnail: "gado-gado.jpg", price: 12000, categoryId: 1 },
+        { id: 11, title: "Es Teh", thumbnail: "es-teh.jpg", price: 5000, categoryId: 2 },
+        { id: 12, title: "Teh Panas", thumbnail: "teh-panas.jpg", price: 5000, categoryId: 2 },
+        { id: 13, title: "Nutrisari", thumbnail: "nutrisari.jpg", price: 5000, categoryId: 2 },
+        { id: 14, title: "Nutrisari Susu", thumbnail: "nutrisari-susu.jpg", price: 8000, categoryId: 2 },
+        { id: 15, title: "Fanta Susu", thumbnail: "fanta-susu.jpg", price: 8000, categoryId: 2 },
+        { id: 16, title: "Air Mineral", thumbnail: "air-mineral.jpg", price: 3000, categoryId: 2 },
+      ]
+    }
+  }
+})
+
+</script>
+
+<style scoped>
+.produk_title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.produk_card {
+  cursor: pointer;
+}
+</style>
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -23,7 +23,7 @@
     </v-app-bar> -->
 
     <v-main>
-      <v-container>
+      <v-container fluid>
         <Nuxt />
       </v-container>
     </v-main>
